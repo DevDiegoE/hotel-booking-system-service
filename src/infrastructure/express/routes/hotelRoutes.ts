0bd8ce616acdc794d6061bd59d1d075f58c39f32
DiegoErquicia <Diego.Erquicia@jala.university>
2025-07-27 23:44:46 -0400

    LB-3 #1. Protect Hotel routes

--- src/infrastructure/express/routes/hotelRoutes.ts
+++ src/infrastructure/express/routes/hotelRoutes.ts
@@ -2,14 +2,16 @@ import { Router } from 'express';
 import { container } from 'tsyringe';
 
 import { validateRequest } from '../middlewares/validateRequest.ts';
+import { authMiddleware } from '../middlewares/authMiddleware.ts';
+
 import { hotelSchema } from '../validators/hotelValidator.ts';
 import { HotelController }from '../controllers/hotelController.ts';
 
 export const hotelRoutes = Router();
 const hotelController = container.resolve(HotelController);
 
-hotelRoutes.post('/', validateRequest(hotelSchema), hotelController.create);
+hotelRoutes.post('/', authMiddleware, validateRequest(hotelSchema), hotelController.create);
 hotelRoutes.get('/', hotelController.getAll);
 hotelRoutes.get('/:id', hotelController.getById);
-hotelRoutes.put('/:id', validateRequest(hotelSchema.partial()), hotelController.update);
-hotelRoutes.delete('/:id', hotelController.deleteById);
+hotelRoutes.put('/:id', authMiddleware, validateRequest(hotelSchema.partial()), hotelController.update);
+hotelRoutes.delete('/:id', authMiddleware, hotelController.deleteById);
